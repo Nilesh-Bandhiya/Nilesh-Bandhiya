97465907b727378967e4b144979304db86ddb72a
Radhe5050 <radheayar5050@gmail.com>
2023-07-08 01:46:08 +0530

    add meta tags for preview

--- src/RandomImage.jsx
+++ src/RandomImage.jsx
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
-import { FaFacebook, FaTwitter, FaWhatsapp } from 'react-icons/fa';
-import './RandomImage.css';
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share";
+import { FaFacebook, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { Helmet } from "react-helmet";
+import "./RandomImage.css";
 
 class RandomImage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      imageUrl: '',
+      imageUrl: "",
     };
   }
 
@@ -19,7 +24,7 @@ class RandomImage extends Component {
 
   fetchRandomImage = async () => {
     try {
-      const response = await axios.get('https://picsum.photos/600/400');
+      const response = await axios.get("https://picsum.photos/600/400");
       this.setState({ imageUrl: response.request.responseURL });
     } catch (error) {
       console.error(error);
@@ -28,10 +33,23 @@ class RandomImage extends Component {
 
   render() {
     const { imageUrl } = this.state;
-    const shareTitle = 'Check out this random image!';
+    const shareTitle = "Check out this random image!";
 
     return (
       <div className="random-image-container">
+        <Helmet>
+          <meta property="og:image" content={imageUrl} />
+          <meta property="og:title" content="Check out this random image!" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content={imageUrl} />
+          <meta name="twitter:title" content="Check out this random image!" />
+          <meta property="og:image" content={imageUrl} />
+          <meta property="og:title" content="Check out this random image!" />
+          <meta
+            property="og:description"
+            content="Description of the shared content."
+          />
+        </Helmet>
         <div className="image-wrapper">
           <img className="random-image" src={imageUrl} alt="Random" />
         </div>
@@ -42,7 +60,7 @@ class RandomImage extends Component {
           <TwitterShareButton url={imageUrl} title={shareTitle}>
             <FaTwitter className="social-icon" />
           </TwitterShareButton>
-          <WhatsappShareButton url={imageUrl} title={shareTitle}>
+          <WhatsappShareButton url={imageUrl} title={shareTitle} data-action="share/whatsapp/share">
             <FaWhatsapp className="social-icon" />
           </WhatsappShareButton>
         </div>
