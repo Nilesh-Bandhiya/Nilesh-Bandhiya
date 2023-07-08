52e9f6e8eca2f815c833677687db38b3ac6f04db
Radhe5050 <radheayar5050@gmail.com>
2023-07-08 13:31:25 +0530

    some changes for preview

--- src/RandomImage.jsx
+++ src/RandomImage.jsx
@@ -24,7 +24,7 @@ class RandomImage extends Component {
 
   fetchRandomImage = async () => {
     try {
-      const response = await axios.get("https://picsum.photos/600/400");
+      const response = await axios.get("https://picsum.photos/400/400");
       this.setState({ imageUrl: response.request.responseURL });
     } catch (error) {
       console.error(error);
@@ -33,40 +33,33 @@ class RandomImage extends Component {
 
   render() {
     const { imageUrl } = this.state;
-    const shareUrl = "https://random-image-share.vercel.app/";
     const shareTitle = "Check out this random image!";
 
     return (
       <>
         <Helmet>
-          {/* Facebook */}
-          <meta property="og:url" content={shareUrl} />
+          <meta property="og:url" content={imageUrl} />
           <meta property="og:title" content={shareTitle} />
           <meta property="og:image" content={imageUrl} />
-
-          {/* Twitter */}
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content={shareTitle} />
           <meta name="twitter:image" content={imageUrl} />
-
-          {/* WhatsApp */}
-          <meta property="og:type" content="website" />
-          <meta property="og:image:alt" content={shareTitle} />
-          <meta property="og:image:type" content="image/jpeg" />
+          {/* <meta property="og:type" content="website" /> */}
+          {/* <meta property="og:image:alt" content={shareTitle} /> */}
         </Helmet>
         <div className="random-image-container">
           <div className="image-wrapper">
             <img className="random-image" src={imageUrl} alt="Random" />
           </div>
           <div className="button-container">
-            <FacebookShareButton url={shareUrl} quote={shareTitle}>
+            <FacebookShareButton url={imageUrl} quote={shareTitle}>
               <FaFacebook className="social-icon" />
             </FacebookShareButton>
-            <TwitterShareButton url={shareUrl} title={shareTitle}>
+            <TwitterShareButton url={imageUrl} title={shareTitle}>
               <FaTwitter className="social-icon" />
             </TwitterShareButton>
             <WhatsappShareButton
-              url={shareUrl}
+              url={imageUrl}
               title={shareTitle}
             >
               <FaWhatsapp className="social-icon" />
